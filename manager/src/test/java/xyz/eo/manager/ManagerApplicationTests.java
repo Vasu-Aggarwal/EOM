ce63d5b01cf75d337594619379df86f00d4408eb
Vasu Aggarwal <e29880@extramarks.com>
2025-05-12 10:49:16 +0530

    intial commit

---
+++ manager/src/test/java/xyz/eo/manager/ManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.eo.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
