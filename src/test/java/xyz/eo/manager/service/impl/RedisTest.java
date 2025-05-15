42eb1904ef8c5f5e4dac8af229c5e9695f645f8a
Vasu Aggarwal <e29880@extramarks.com>
2025-05-15 16:05:59 +0530

    redis setup

---
+++ src/test/java/xyz/eo/manager/service/impl/RedisTest.java
@@ -0,0 +1,22 @@
+package xyz.eo.manager.service.impl;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("dev")
+public class RedisTest {
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    void testRedis(){
+        redisTemplate.opsForValue().set("email", "test@test.com");
+        Object email = redisTemplate.opsForValue().get("email");
+        int a= 1;
+    }
+}
